04223229afb0aebef9abd0906eefa7f86426f8b3
Brandon Risberg <brandon.risberg@gmail.com>
2017-04-17 03:18:22 -0700

    mod compatibility

--- mod/modinfo.lua
+++ mod/modinfo.lua
@@ -13,6 +13,9 @@ forumthread = ""
 
 -- This lets other players know if your mod is out of date, update it to match the current version in the game
 api_version = 6
+dont_starve_compatible = true
+reign_of_giants_compatible = true
+shipwrecked_compatible = true
 
 -- Can specify a custom icon for this mod!
 -- icon_atlas = "modicon.xml"
--- mod/scripts/tweaks/tweakteenbird.lua
+++ mod/scripts/tweaks/tweakteenbird.lua
@@ -2,8 +2,8 @@
 local function SpawnTameAdult(inst)
 	--print("TameTallbirds - SpawnTameAdult called")
 
-  -- local tamebird = GLOBAL.SpawnPrefab("tametallbird")
-	local tamebird = GLOBAL.SpawnPrefab("spider")
+  local tamebird = SpawnPrefab("tametallbird")
+	-- local tamebird = SpawnPrefab("spider")
   tamebird.Transform:SetPosition(inst.Transform:GetWorldPosition())
   tamebird.sg:GoToState("idle")
 
@@ -17,17 +17,17 @@ end
 function tweak_teenbird(inst)
   local SpawnNormalAdult = inst.userfunctions.SpawnAdult
   function inst.userfunctions.SpawnAdult(inst)
-    SpawnTameAdult(inst)
-    -- if
-    --   inst.components.follower
-    --   and inst.components.follower.leader
-    --   and inst.components.follower.leader:HasTag("player") then
-    --   -- We are a teen following a player, spawn a tame adult
-    --   SpawnTameAdult(inst)
-    -- else
-    --   -- We are a wild bird, spawn normal adult
-    --   SpawnNormalAdult(inst)
-    -- end
+    -- SpawnTameAdult(inst)
+    if
+      inst.components.follower
+      and inst.components.follower.leader
+      and inst.components.follower.leader:HasTag("player") then
+      -- We are a teen following a player, spawn a tame adult
+      SpawnTameAdult(inst)
+    else
+      -- We are a wild bird, spawn normal adult
+      SpawnNormalAdult(inst)
+    end
   end
 end
 
