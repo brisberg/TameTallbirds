f10e7202ef2acd9b8a846f11db94bc86f7970060
brisberg <brandon.risberg@gmail.com>
2017-06-01 02:03:46 -0700

    en lang file

---
+++ mod/scripts/lang/en-us.lua
@@ -0,0 +1,10 @@
+return {
+	TAMETALLBIRD_NAME = 'Tame Tallbird',
+	TAMETALLBIRD_DESCRIBE = {
+	    FULL = "Surprisingly friendly with a full stomach.",
+	    GENERIC = "Very tall and very loyal.",
+	    HUNGRY = "Better get you some food, Big Guy.",
+	    STARVING = "Careful Big Guy, just don't eat me!",
+	    SLEEPING = "Curiously docile.",
+	}
+}
