d3f1615b3919069c544475b7b777cfbe4b3ae28d
brisberg <brandon.risberg@gmail.com>
2017-06-21 20:25:26 -0700

    added localization labels

--- mod/scripts/lang/en-us.lua
+++ mod/scripts/lang/en-us.lua
@@ -1,14 +1,22 @@
 return {
-	TAMETALLBIRD_NAME = 'Tame Tallbird',
-	TAMETALLBIRD_DESCRIBE = {
-	    FULL = "Surprisingly friendly with a full stomach.",
-	    GENERIC = "Very tall and very loyal.",
-	    HUNGRY = "Better get you some food, Big Guy.",
-	    STARVING = "Careful Big Guy, just don't eat me!",
-	    SLEEPING = "Curiously docile.",
-	},
-	STAY_ACTION = {
-		NAME = "Stay",
-		ANNOUNCE = "Stay here!",
-	},
+    -- Display name of the tame birds
+    TAMETALLBIRD_NAME = 'Tame Tallbird',
+    TAMETALLBIRD_DESCRIBE = {
+       -- Birds are friendly when well fed
+        FULL = "Surprisingly friendly with a full stomach.",
+        -- Generic inspect string for the bird
+        GENERIC = "Very tall and very loyal.",
+        -- Bird is hungry and needs food
+        HUNGRY = "Better get you some food, Big Guy.",
+        -- Bird is hungry and might attack the player
+        STARVING = "Careful Big Guy, just don't eat me!",
+        -- Bird is asleep
+        SLEEPING = "Curiously docile.",
+    },
+    STAY_ACTION = {
+        -- Display name for Right-Click "Stay" action
+        NAME = "Stay",
+        -- Spoken command to a pet to "Stay here"
+        ANNOUNCE = "Stay here!",
+    },
 }
