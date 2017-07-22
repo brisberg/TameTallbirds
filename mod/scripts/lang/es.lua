4ac0fe8aa85e37243980e2f686d45a89bd6887df
Brandon Risberg <brandon.risberg@gmail.com>
2017-07-22 12:20:29 -0700

    added spanish language file

---
+++ mod/scripts/lang/es.lua
@@ -0,0 +1,22 @@
+return {
+    -- Display name of the tame birds
+    TAMETALLBIRD_NAME = 'Pájaralto domesticado',
+    TAMETALLBIRD_DESCRIBE = {
+        -- Birds are friendly when well fed
+        FULL = "Sorprendentemente amable con un estómago lleno.",
+        -- Generic inspect string for the bird
+        GENERIC = "Muy alto y muy leal.",
+        -- Bird is hungry and needs food
+        HUNGRY = "Mejor que te traiga comida, Big Guy.",
+        -- Bird is hungry and might attack the player
+        STARVING = "Cuidadoso Big Guy, simplemente no me comen!",
+        -- Bird is asleep
+        SLEEPING = "Curiosamente dócil.",
+    },
+    STAY_ACTION = {
+        -- Display name for Right-Click "Stay" action
+        NAME = "Permanecer",
+        -- Spoken command to a pet to "Stay here"
+        ANNOUNCE = "¡Quédate aquí!",
+    },
+}
