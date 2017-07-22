8b8153b03a2146d9d327972e4d65451cf47d7851
Brandon Risberg <brandon.risberg@gmail.com>
2017-07-22 12:39:28 -0700

    Antonio's translations initial

--- mod/scripts/lang/es.lua
+++ mod/scripts/lang/es.lua
@@ -1,15 +1,15 @@
 return {
     -- Display name of the tame birds
-    TAMETALLBIRD_NAME = 'Pájaralto domesticado',
+    TAMETALLBIRD_NAME = "Pájaralto domesticado",
     TAMETALLBIRD_DESCRIBE = {
         -- Birds are friendly when well fed
         FULL = "Sorprendentemente amable con un estómago lleno.",
         -- Generic inspect string for the bird
         GENERIC = "Muy alto y muy leal.",
         -- Bird is hungry and needs food
-        HUNGRY = "Mejor que te traiga comida, Big Guy.",
+        HUNGRY = "Mejor te traigo algo de comer, muchachote.",
         -- Bird is hungry and might attack the player
-        STARVING = "Cuidadoso Big Guy, simplemente no me comen!",
+        STARVING = "Cuidado muchachote, no mas no me comas!",
         -- Bird is asleep
         SLEEPING = "Curiosamente dócil.",
     },
